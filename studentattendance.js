52d4b284a551b2faed14887b37b234e14d69ecdd
jay-rajapure <jayrajapure758@gmail.com>
2024-12-24 01:08:11 +0530

    Initial commit

---
+++ studentattendance.js
@@ -0,0 +1,17 @@
+//object 
+//for in loop 
+
+let student1 = {
+
+    name:" jay rajapure",
+    age:20,
+    attendance:100
+
+}
+
+console.log("hello fuck")
+for  (let key in student1){
+
+console.log(key)
+
+}
